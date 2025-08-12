33b6630bc24ec4b023e994f036f90f55aa27d408
Ami0480 <amifuku80@gmail.com>
2025-08-12 11:11:29 +0800

    Created current weather js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,47 @@
 function showElement(response) {
+  console.log(response.data);
   let searchCity = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#current-temperature");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let descriptionElement = document.querySelector("#description");
+  let dateElement = document.querySelector("#date");
+  let dt = response.data.dt;
+  let timezone = response.data.timezone;
+  let date = new Date((dt + timezone) * 1000);
+  let iconElement = document.querySelector("#icon");
+  let iconCode = response.data.weather[0].icon;
+  let iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
   searchCity.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  dateElement.innerHTML = formatDate(date);
+  iconElement.src = iconUrl;
+  iconElement.alt = response.data.weather[0].description;
+}
+
+function formatDate(date) {
+  let hour = date.getUTCHours();
+  let minute = date.getUTCMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getUTCDay()];
+
+  if (hour < 10) hour = `0${hour}`;
+  if (minute < 10) minute = `0${minute}`;
+
+  return `${day} ${hour}:${minute}`;
 }
 
 function displayCity(city) {
